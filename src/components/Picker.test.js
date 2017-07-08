d5d88cf270a3399449de46dfddc262e367b0d67a
Chris Trinh <ChrisT11@verifone.com>
2017-07-07 21:04:41 -0700

    Add snapshot testing and other test for components

--- src/components/Picker.test.js
+++ src/components/Picker.test.js
@@ -3,13 +3,33 @@ import Picker from './Picker';
 
 it('renders without crashing', () => {
   const selectedSubreddit = 'frontend';
-  const handleChange = () => {};
+  const handleChange = jest.fn();
 
-  shallow(
+  const wrapper = shallow(
     <Picker
       value={selectedSubreddit}
       onChange={handleChange}
       options={['reactjs', 'frontend']}
     />
   );
+
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('should handle select change event', () => {
+  const selectedSubreddit = 'frontend';
+  const mockedEvent = { target: {} };
+  const handleChange = jest.fn();
+
+  const wrapper = shallow(
+    <Picker
+      value={selectedSubreddit}
+      onChange={handleChange}
+      options={['reactjs', 'frontend']}
+    />
+  );
+
+  wrapper.find('select').simulate('change', mockedEvent);
+
+  expect(handleChange).toHaveBeenCalled();
 });
--- src/components/Posts.test.js
+++ src/components/Posts.test.js
@@ -2,6 +2,12 @@ import React from 'react';
 import Posts from './Posts';
 
 it('renders without crashing', () => {
-  const posts = [];
-  shallow(<Posts posts={posts} />);
+  const posts = [
+    { url: 'http://example.com/blah', title: 'blah' },
+    { url: 'http://example.com/others', title: 'others' }
+  ];
+
+  const wrapper = shallow(<Posts posts={posts} />);
+
+  expect(wrapper).toMatchSnapshot();
 });

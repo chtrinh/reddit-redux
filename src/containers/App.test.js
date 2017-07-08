116b130b3641164e4f414c1e7868fdbc20b8d94c
Chris Trinh <ChrisT11@verifone.com>
2017-07-07 21:07:07 -0700

    Add snapshot testing for containers

--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
-
 import App from './App';
 
 it('renders without crashing', () => {
-  shallow(<App />);
+  const selectedSubreddit = 'frontend';
+  const handleChange = jest.fn();
+
+  const wrapper = shallow(<App />);
+
+  expect(wrapper).toMatchSnapshot();
 });
